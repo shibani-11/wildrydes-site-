a1ab3a05241973c00dcd7db1ee256f83a8f595a0
Shibani Kumar <shibanik11@gmail.com>
2026-05-04 17:55:16 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_I5Jdepol8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1upce90b4mv7j387ic7lcs87cl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
